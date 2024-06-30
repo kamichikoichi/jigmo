0052c107bdd6fc6db0dfd74d73221f692d3cc304
Koichi Kamichi <kamichi@ic.daito.ac.jp>
2024-07-01 00:12:37 +0900

    Ext.IグリフをunstableからコードポイントのSVGを取得するように更新

--- getglyphs.py
+++ getglyphs.py
@@ -34,10 +34,11 @@ for i in codepoints:
     d2 = c[:-2]
     print("\r", i, sep="", end="        ")
     if not base + "/" + d1 + "/" + d2 + "/" + i + ".svg" in svg:
-        if 0x2ebf0 <= int(c[1:], 16) <= 0x2ee5d or 0x2ffc <= int(c[1:], 16) <= 0x2fff or c == "u31ef":
-            url = "https://glyphwiki.org/glyph/unstable-" + i + ".svg"
-        else:
-            url = "https://glyphwiki.org/glyph/" + i + ".svg"
+        #if 0x2ebf0 <= int(c[1:], 16) <= 0x2ee5d or 0x2ffc <= int(c[1:], 16) <= 0x2fff or c == "u31ef":
+        #    url = "https://glyphwiki.org/glyph/unstable-" + i + ".svg"
+        #else:
+        #    url = "https://glyphwiki.org/glyph/" + i + ".svg"
+        url = "https://glyphwiki.org/glyph/" + i + ".svg"
         count = 20
         while count > 0:
             data = requests.get(url).content
